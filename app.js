d015be81ed048a34574d7d7da0c3edd5c55db683
rafaelmarch3 <rafaelmarch3@gmail.com>
2024-07-08 08:08:15 -0300

    Updating

--- app.js
+++ app.js
@@ -116,7 +116,7 @@ new Chart(document.getElementById("line-chart"), {
           fill: false,
           lineTension: 0, // Set lineTension to 0 for no interpolation
         }, { 
-          data: [3,3,3,3,3,6,6,6,7,7,10],
+          data: [3,3,3,3,3,6,6,6,7,7,10,11,14,17],
           label: "Realizado",
           borderColor: "#8e5ea2",
           borderWidth: 8,
