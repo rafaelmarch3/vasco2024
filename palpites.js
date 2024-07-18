85cb82fb4508bebf6ea20778beb524a450c90daa
rafaelmarch3 <rafaelmarch3@gmail.com>
2024-07-18 08:12:50 -0300

    Updating

--- palpites.js
+++ palpites.js
@@ -44,7 +44,7 @@ for(let i = 0; i < nPlayers; i++) {
 console.log(playersPoints);
 
 // Match results: update during the championship
-results = ["V", "D", "D", "D", "D", "V", "D", "D", "E", "D", "V", "D", "E", "V", "V", "", "", "", ""];
+results = ["V", "D", "D", "D", "D", "V", "D", "D", "E", "D", "V", "D", "E", "V", "V", "V", "V", "", ""];
 
 // Actual points: update during the championship
 actualPoints = "0";
